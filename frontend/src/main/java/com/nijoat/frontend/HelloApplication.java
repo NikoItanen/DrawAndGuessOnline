7724d99d54d41f9c51d8eabf1ebd3ebf3f658b63
Niko Itänen <120341039+NikoItanen@users.noreply.github.com>
2024-03-23 23:28:01 +0200

    Environment assembled!
    Backend and frontend functionalities are dived into their own modules.

--- frontend/src/main/java/com/nijoat/frontend/HelloApplication.java
+++ frontend/src/main/java/com/nijoat/frontend/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.drawandguessonline;
+package com.nijoat.frontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Environment Assembled!");
         stage.setScene(scene);
         stage.show();
     }
---
+++ frontend/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.nijoat.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.nijoat.frontend to javafx.fxml;
+    exports com.nijoat.frontend;
+    exports com.nijoat.frontend.controller;
+    opens com.nijoat.frontend.controller to javafx.fxml;
+}
