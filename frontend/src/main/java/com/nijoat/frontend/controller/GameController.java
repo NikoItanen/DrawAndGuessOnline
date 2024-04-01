7c9d16d66af33c09a00d6818c26aa99ced00ba3b
Niko Itänen <120341039+NikoItanen@users.noreply.github.com>
2024-04-01 18:38:09 +0300

    Testing Gameroom added!

---
+++ frontend/src/main/java/com/nijoat/frontend/controller/GameController.java
@@ -0,0 +1,14 @@
+package com.nijoat.frontend.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class GameController {
+
+}
--- frontend/src/main/java/com/nijoat/frontend/controller/LobbyController.java
+++ frontend/src/main/java/com/nijoat/frontend/controller/LobbyController.java
@@ -1,5 +1,6 @@
 package com.nijoat.frontend.controller;
 
+import com.almasb.fxgl.entity.action.Action;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +17,19 @@ public class LobbyController {
     protected void onLogoutButtonClick(ActionEvent event) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/nijoat/frontend/login-view.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            currentStage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void testRoomButtonClick(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/nijoat/frontend/game-view.fxml"));
             Parent root = fxmlLoader.load();
             Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
