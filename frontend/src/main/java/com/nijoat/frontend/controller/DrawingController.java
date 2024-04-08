208c2424226faea18cba9276b90ee90830b7b484
akseliteuho <ajteuh@utu.fi>
2024-04-08 19:50:56 +0300

    Drawing ability completed
    Drawing ability completed

--- frontend/src/main/java/com/nijoat/frontend/controller/DrawingController.java
+++ frontend/src/main/java/com/nijoat/frontend/controller/DrawingController.java
@@ -3,6 +3,7 @@ package com.nijoat.frontend.controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.control.CheckBox;
@@ -20,6 +21,22 @@ public class DrawingController {
 
     @FXML
     private CheckBox eraser;
+
+    public void initialize() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseDragged(e -> {
+            double size = Double.parseDouble(brushSize.getText());
+            gc.setFill(colorPicker.getValue());
+
+            if (eraser.isSelected()) {
+                gc.clearRect(e.getX() - size / 2, e.getY() - size / 2, size, size);
+            } else {
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(e.getX() - size / 2, e.getY() - size / 2, size, size);
+            }
+        });
+    }
 
     public void onExit() {
         Platform.exit();
